7c56c3539af7bcaa799d9b1a08c26b4cf687d6e2
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-06-25 12:41:52 +0530

    dataui

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Hero from './components/Hero';
 import Navbar from './components/Navbar';
 import Newsletter from './components/Newsletter';
 
+
 function App() {
   return (
     <div>
@@ -15,6 +16,7 @@ function App() {
       <Newsletter />
       <Cards />
       <Footer />
+      
     </div>
   );
 }

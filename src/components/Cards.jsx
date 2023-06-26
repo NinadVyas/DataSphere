298f4e1ea2bdc02cb1792e6d263db0574645d88b
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-06-26 15:43:30 +0530

    dataui

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Single from '../assets/single.png'
 import Double from '../assets/double.png'
 import Triple from '../assets/triple.png'
+import Analytics from './Analytics';
 
 const Cards = () => {
   return (
@@ -9,8 +10,8 @@ const Cards = () => {
       <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8'>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Single} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              <h2 className='text-2xl font-bold text-center py-8'>Managment+Service</h2>
+              <p className='text-center text-4xl font-bold'>$249</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
                   <p className='py-2 border-b mx-8'>1 Granted User</p>
@@ -20,7 +21,7 @@ const Cards = () => {
           </div>
           <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-transparent' src={Double} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
+              <h2 className='text-2xl font-bold text-center py-8'>Managment</h2>
               <p className='text-center text-4xl font-bold'>$149</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
@@ -31,8 +32,8 @@ const Cards = () => {
           </div>
           <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
               <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="/" />
-              <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
-              <p className='text-center text-4xl font-bold'>$149</p>
+              <h2 className='text-2xl font-bold text-center py-8'>Managment+Services+Analytics </h2>
+              <p className='text-center text-4xl font-bold'>$449</p>
               <div className='text-center font-medium'>
                   <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
                   <p className='py-2 border-b mx-8'>1 Granted User</p>

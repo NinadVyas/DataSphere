cc1bea4817462f092f70365a3a23bf0f4a0f770b
Ninad Vyas <115214615+NinadVyas@users.noreply.github.com>
2023-07-17 18:18:34 +0530

    dataui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
   return (
     <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
       <div>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>DataSphere</h1>
+        <h1 className='w-full text-3xl font-bold text-[#4ac6ff'>DataSphere</h1>
         <p className='py-4'>Accelerating business growth through rapid data analysis and agile decision-making.</p>
         <div className='flex justify-between md:w-[75%] my-6'>
             <FaFacebookSquare size={30} />
